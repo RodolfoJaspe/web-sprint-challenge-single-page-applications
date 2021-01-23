731a42a6fa118a78b8874e644695a4bc3831b4bc
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-01-23 01:10:55 -0500

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { Link, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Pizza from './components/Pizza';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+
+    <div>
+        <div>
+            <h1>Lambda Eats</h1>    
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/pizza">Pizza</Link>
+            </div>
+        </div>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/pizza" component={Pizza} />
+        </Switch>
+    </div>
+   
+    
+      
+
   );
 };
 export default App;
